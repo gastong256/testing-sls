9d85bde0023e14c65dd1117535e7ae79b2bf3f30
gaston gonzalez <gaston.gonzalez.256@gmail.com>
2026-02-23 04:52:34 -0300

    testing sls

---
+++ handler.js
@@ -0,0 +1,29 @@
+'use strict';
+
+module.exports.hello = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
+
+module.exports.helloUser = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: `Hello ${event.pathParameters.name}. You're testing sls.`,
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+};
